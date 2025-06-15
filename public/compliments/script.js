a5c81bbd020a1d63146038d8f2c06468f8f87f95
BestCactus <vojtovidlo.f@gmail.com>
2025-06-15 12:15:11 +0200

    open when cards page done

--- public/compliments/script.js
+++ public/compliments/script.js
@@ -100,66 +100,6 @@ $("#compliment-form").submit(function (e) {
     });
 });
 
-// $("#close-popup, #popup-overlay").on("click", function () {
-//   $("#popup-overlay, #popup").fadeOut(200);
-// });
-
-$(".select-btn").click(function () {
-  $(".select-btn").removeClass("selected");
-  $(this).addClass("selected");
-
-  if ($(this).attr("id") === "vojtisek-btn") {
-    $("#question").text("What is the most favorite plushie of Hanicka?");
-  }
-
-  if ($(this).attr("id") === "hanicka-btn") {
-    $("#question").text("What is the nickname that Hanicka calls Vojtik?");
-  }
-});
-$("#login-form").submit(function (e) {
-  const handleSuccess = () => {
-    $("#submit-login").addClass("success").attr("value", "Done!");
-  };
-
-  const handleFail = (error) => {
-    $("#submit-login").addClass("error").attr("value", "Failed!");
-  };
-
-  e.preventDefault();
-
-  $("#submit-login")
-    .attr("value", "Loading...")
-    .removeClass("error")
-    .removeClass("success");
-
-  const formData = $(this).serialize();
-  let appendToFormData;
-  if ($("#vojtisek-btn").hasClass("selected")) {
-    appendToFormData = "&person=vojtik";
-  }
-
-  if ($("#hanicka-btn").hasClass("selected")) {
-    appendToFormData = "&person=hanca";
-  }
-
-  $.post("/login", formData + appendToFormData)
-    .done((response) => {
-      if (response.error) {
-        return handleFail();
-      }
-      if (response.success) {
-        handleSuccess();
-        $("#popup-overlay, #popup").fadeOut(200);
-        getCompliment();
-      }
-    })
-    .fail((error) => {
-      handleFail(error);
-    });
-});
-
-//IMPORTANT: With load update reactions correctly
-
 $(".reaction-btn").click(function () {
   const formData = `complimentId=${currectCompliment._id}&reaction=${$(
     this
